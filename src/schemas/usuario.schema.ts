cc59d4ccd3b488b49fd481f2424fadaf9819908b
9erikSantos6 <9xerix6@gmail.com>
2026-02-10 16:14:38 -0300

    fix: Corrige nome da constante usuarioRegisterInputSchema para UsuarioRegisterInputSchema

--- src/schemas/usuario.schema.ts
+++ src/schemas/usuario.schema.ts
@@ -21,7 +21,7 @@ export const UsuarioLoginInputSchema = z
   })
   .strict();
 
-export const usuarioRegisterInputSchema = z
+export const UsuarioRegisterInputSchema = z
   .object({
     nome: z.string().trim().min(3, 'O nome deve ter no mínimo 3 caracteres'),
     email: z.email('Email inválido').trim(),
--- src/types/types/usuario.types.ts
+++ src/types/types/usuario.types.ts
@@ -3,12 +3,12 @@ import type {
   UsuarioInputSchema,
   UsuarioLoginInputSchema,
   UsuarioOutputSchema,
+  UsuarioRegisterInputSchema,
   UsuarioUpdateInputSchema,
-  usuarioRegisterInputSchema,
 } from '@/schemas';
 
 export type UsuarioInput = z.infer<typeof UsuarioInputSchema>;
 export type UsuarioUpdate = z.infer<typeof UsuarioUpdateInputSchema>;
 export type UsuarioOutput = z.infer<typeof UsuarioOutputSchema>;
 export type UsuarioLoginInput = z.infer<typeof UsuarioLoginInputSchema>;
-export type UsuarioRegisterInput = z.infer<typeof usuarioRegisterInputSchema>;
+export type UsuarioRegisterInput = z.infer<typeof UsuarioRegisterInputSchema>;
