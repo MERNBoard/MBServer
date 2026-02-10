0a43125e0850288dfa9d584613f0a05fc52daa8a
9erikSantos6 <9xerix6@gmail.com>
2026-02-10 16:02:13 -0300

    feat: Adiciona serviço de lembrete e exportações no índice de serviços

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export * from './auth.token.service';
+export * from './lembrete.service';
+export * from './notificacao.service';
+export * from './tarefa.service';
+export * from './usuario.service';
